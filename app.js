2a534527dfe46b7c99ee96e3960d5d2c6cf1bfcf
Surfer5437 <livingstonebrandon5437@gmail.com>
2023-02-16 21:18:46 -0500

    added logic for click events

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const player2 = {
     color:"blue",
     wins:0
 };
-const curPlayer=player1;
+let curPlayer=player1;
 const board = document.querySelector(".board");
-const slot = document.querySelector("#token");
-board.addEventListener("click", e=>{
-   if (e.target=slot){
-    
-e.target.classList.remove('slot');
-e.target.classList.add('played');
-e.target.style.backgroundColor=curPlayer.color;
-if (curPlayer===player1){
-    curPlayer=player2();
-    console.log()
+const slots = document.querySelectorAll("div");
+for (slot of slots) {
+    slot.addEventListener("click", e => {
+        if (!e.target.classList.contains('played')) {
+            e.target.classList.remove('slot');
+            e.target.classList.add('played');
+            e.target.style.backgroundColor = curPlayer.color;
+            if (curPlayer === player1) {
+                curPlayer = player2;
+                console.log()
+            }
+            else if (curPlayer === player2) {
+                curPlayer = player1;
+            };
+        }
+    })
 }
-else if(curPlayer===player2){
-    curPlayer=player1;
-}
-console.log(e.target);
-   }
-})
